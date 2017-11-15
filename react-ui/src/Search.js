0245accc0d9aa9891c8a536715dd9166e6481f0a
Prashanth <yvprashanth@gmail.com>
2017-11-14 23:04:42 -0800

    small change to comment

--- react-ui/src/Search.js
+++ react-ui/src/Search.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react'
 
 class Search extends Component {
+
+
+
   render () {
     const { showSearchPage } = this.props
 
@@ -10,13 +13,13 @@ class Search extends Component {
           <a className='close-search' onClick={showSearchPage}>Close</a>
           <div className='search-books-input-wrapper'>
             {/*
-                                                                                                  NOTES: The search from BooksAPI is limited to a particular set of search terms.
-                                                                                                  You can find these search terms here:
-                                                                                                  https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
+              NOTES: The search from BooksAPI is limited to a particular set of search terms.
+              You can find these search terms here:
+              https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
 
-                                                                                                  However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
-                                                                                                  you don't find a specific author or title. Every search is limited by search terms.
-                                                                                                */}
+              However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
+              you don't find a specific author or title. Every search is limited by search terms.
+            */}
             <input type='text' placeholder='Search by Title or Author' />
           </div>
         </div>
